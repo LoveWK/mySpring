a7f3a5419e0d18e057477ffe11d2181baca74d02
LoveWK <1093507689@qq.com>
2020-03-08 19:22:31 +0800

    自己编译的spring源码

--- myIocDemo/src/main/java/com/wk/test/Test.java
+++ myIocDemo/src/main/java/com/wk/test/Test.java
@@ -4,6 +4,9 @@ import com.wk.annotation.MyAnnotation;
 import com.wk.service.IndexService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+/**
+ * 测试类
+ */
 public class Test {
 	public static void main(String[] args) {
 		//把spring所有的前提环境准备好了，包括spring容器还有类的实例化都完成了。
